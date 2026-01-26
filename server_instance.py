929f67ced5255c799cf0894e56c42824e00bf4fc
JonathanKosasih18 <jonathankosasih@gmail.com>
2026-01-26 16:03:58 +0700

    Allowed Hosts Tweak Again

--- server_instance.py
+++ server_instance.py
@@ -4,6 +4,7 @@ from mcp.server.transport_security import TransportSecuritySettings
 mcp = FastMCP(
     "CTBA MCP",
     transport_security=TransportSecuritySettings(
+        enable_dns_rebinding_protection=False,
         allowed_hosts=["*"] 
     )
 )
